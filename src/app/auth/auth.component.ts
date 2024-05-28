17c7199b5e6bbe635679429e2a8df47aedcc4cb2
bhargavi8055 <59086546+bhargavi8055@users.noreply.github.com>
2024-05-28 22:05:30 +0530

    Update auth.component.ts
    used MaterialUI module to improve the popup for the invalid credentials.

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,87 +1,88 @@
-import { Component, ComponentFactoryResolver, OnDestroy, ViewChild } from "@angular/core";
+import { Component, OnDestroy, ViewChild, ViewContainerRef } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { AuthResponseData, AuthService } from "./auth.service";
 import { Observable, Subscription } from "rxjs";
 import { Router } from "@angular/router";
 import { AlertComponent } from "../shared/alert/alert.component";
-import { PlaceholderDirective } from "../shared/placeholder/placeholder.directive";
+import { PlaceholderDirective } from "../shared/placeholder/PlaceholderDirective";
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from '../dialog/dialog.component';
 
 @Component({
-    selector:'app-auth',
-    templateUrl:'./auth.component.html',
-    styleUrls:['./auth.component.css']
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
 })
-export class AuthComponent implements OnDestroy{
+export class AuthComponent implements OnDestroy {
 
-    isLoginMode = true;
-    isLoading = false;
-    error:string = null;
+  isLoginMode = true;
+  isLoading = false;
+  error: string = null;
 
-    @ViewChild(PlaceholderDirective,{static:false}) alertHost:PlaceholderDirective;
+  @ViewChild(PlaceholderDirective, { static: false }) alertHost: PlaceholderDirective;
+
+  private closeSub: Subscription;
+
+  constructor(private authService: AuthService, private router: Router,public dialog: MatDialog) { }
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) { return; }
+
+    const email = form.value.email;
+    const password = form.value.password;
+
+    let authObs: Observable<AuthResponseData>;
+
+    this.isLoading = true;
+    if (this.isLoginMode) {
+      authObs = this.authService.login(email, password);
+    } else {
+      authObs = this.authService.signUp(email, password);
+    }
     
-    private closeSub:Subscription;
+    authObs.subscribe(resData => {
+      console.log(resData);
+      this.isLoading = false;
+      this.router.navigate(['/recipes']);
+    }, errorMessage => {
+      console.log(errorMessage);
+      this.error = errorMessage;
+    //   this.showErrorAlert(errorMessage);
+      this.isLoading = false;
+      this.openDialog();
+      // alert("password is incorrect")
+    });
 
-    constructor(private authService:AuthService,private router:Router,private componentFactoryResolver:ComponentFactoryResolver
-        ){}
-    onSwitchMode(){
-        this.isLoginMode = !this.isLoginMode;
+    form.reset();
+  }
+  openDialog(): void {
+    this.dialog.open(DialogComponent);
+  }
+  onHandleError() {
+    this.error = null;
+  }
+
+  ngOnDestroy(): void {
+    if (this.closeSub) {
+      this.closeSub.unsubscribe();
     }
-    onSubmit(form:NgForm){
+  }
 
-        if(!form.valid){return;}
+  private showErrorAlert(message: string) {
+    const hostRef = this.alertHost.viewContainerRef;
 
-        const email = form.value.email;
-        const password = form.value.password;
+    hostRef.clear();
 
-        let authObs:Observable<AuthResponseData>;
+    const componentRef = hostRef.createComponent(AlertComponent);
 
-        this.isLoading=true
-        if(this.isLoginMode){
-            authObs = this.authService.login(email,password);
-        }
-        else{
-            authObs = this.authService.signUp(email,password)
-    
-        }
-        authObs.subscribe(resData=>{
-            console.log(resData);
-            this.isLoading=false;
-            this.router.navigate(['/recipes']);
-        },errorMessage=>{
-            
-            console.log(errorMessage);
-            this.error=errorMessage;
-            this.showErrorAlert(errorMessage)
-            this.isLoading=false;
-
-        });
-        
-        form.reset();
-
-    }
-    onHandleError(){
-        this.error = null;
-    }
-    ngOnDestroy(): void {
-        if(this.closeSub){
-            this.closeSub.unsubscribe();
-        }
-    }
-    private showErrorAlert(message:string){
-
-        const alertCmpFactory= this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
-
-        const hostRef = this.alertHost.view;
-
-        hostRef.clear();
-
-       const comRef= hostRef.createComponent(alertCmpFactory);
-
-       comRef.instance.message = message;
-
-       this.closeSub=comRef.instance.close.subscribe(()=>{
-        this.closeSub.unsubscribe();
-        hostRef.clear();
-       })
-    }
+    componentRef.instance.message = message;
+    this.closeSub = componentRef.instance.close.subscribe(() => {
+      this.closeSub.unsubscribe();
+      hostRef.clear();
+    });
+  }
 }
